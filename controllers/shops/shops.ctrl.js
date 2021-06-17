a9edbe93e48f0084888706f641f65d3ab3d400cc
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-06-17 22:57:42 +0900

    장바구니 아이콘 기능 구현

--- controllers/shops/shops.ctrl.js
+++ controllers/shops/shops.ctrl.js
@@ -5,15 +5,19 @@ exports.get_shops_detail = async (req, res) => {
     try {
 
         const shop = await models.Shops.findOne({
-            where : {
-                id : req.params.id
-            },
-            include : [
-                'Menu'
-            ]
-        })
+            where : { id : req.params.id },
+            include : [ 'Menu' ]
+        });
+
+        let cartList = {};
+        let cartLength = 0;                 //장바구니에 담긴 메뉴의 개수
+
+        if( typeof(req.cookies.cartList) !== 'undefined' ){         //request 의 cookie로 쿠키 정보 접근 가능
+            cartList = JSON.parse(unescape(req.cookies.cartList));
+            cartLength = Object.keys(cartList).length;              //배열이 아닌 오브젝트의 갯수는 이와 같이 사용함
+        }
     
-        res.render('shops/detail.html', {shop});
+        res.render('shops/detail.html', {shop, cartLength});
             
     } catch (e) {
         console.log(e);
