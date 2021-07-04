eb1942e3e0183ec1c6844cb47c8f5cdc46d366a4
Kang, Kug-chin <tradeoff0626@gmail.com>
2021-07-04 15:06:22 +0900

    모바일 결제 가능하도록 개선(GET방식) [테스트X]

--- controllers/checkout/checkout.ctrl.js
+++ controllers/checkout/checkout.ctrl.js
@@ -21,6 +21,68 @@ exports.index = (req, res) => {
 };
 
 
+//iamport 결제 처리를 개발자 모드와 같이 사용자가 볼수 있는 상황을 배제하기 위해 컨트롤러에서 처리하도록 개선
+exports.get_complete = async (req, res) => {
+
+    const dotenv = require('dotenv');
+    dotenv.config(); //LOAD CONFIG
+
+    // 모듈 선언
+    const { Iamporter } = require('iamporter');
+    const iamporter = new Iamporter({
+        apiKey: process.env.IAMPORT_APIKEY ,
+        secret: process.env.IAMPORT_SECRET
+    });
+
+    try{
+
+        //iamport에서 결제 정보를 조회. 조회한 정보를 이용하여 아래에 DB로 저장하는 처리를 함. 
+        const iamportData = await iamporter.findByImpUid(req.query.imp_uid);
+
+        //iamport에서 결제 정보를 DB에 저장하는 처리
+        const checkout = await models.Checkout.create({
+            imp_uid : iamportData.data.imp_uid,
+            merchant_uid : iamportData.data.merchant_uid,
+            paid_amount : iamportData.data.amount,
+            apply_num : iamportData.data.apply_num,
+            
+            buyer_email : iamportData.data.buyer_email,
+            buyer_name : iamportData.data.buyer_name,
+            buyer_tel : iamportData.data.buyer_tel,
+            buyer_addr : iamportData.data.buyer_addr,
+            buyer_postcode : iamportData.data.buyer_postcode,
+            shop_id : req.query.shop_id,
+            status : "결재완료",
+        });
+
+        const menuArray = JSON.parse(req.query.menuArray);
+
+        //메뉴 저장 처리 함수 구현(중첩 try~catch 처리 구현. 외부로 예외 throw)
+        async function asyncSetMenu(menu_id){
+            try{
+            const menu = await models.ShopsMenu.findByPk( menu_id );
+            const status = await checkout.addMenu(menu);
+            if(typeof status == 'undefined'){
+                throw `menu :: ${menu_id}가 존재하지 않습니다.`;
+            }
+            }catch(e){
+            throw e;
+            }
+        }
+
+        //주문할 메뉴 수 만큼 저장 처리 함수 반복 수행. 함수안에서 try~catch 중복 처리를 하기 번거로우므로 함수로 구현.
+        for (const menu_id of menuArray) await asyncSetMenu(menu_id);
+
+        res.redirect('/checkout/success');
+
+
+    }catch(e){
+        console.log(e);
+    }
+
+};
+
+
 exports.post_complete = async (req, res) => {
 
     try{
